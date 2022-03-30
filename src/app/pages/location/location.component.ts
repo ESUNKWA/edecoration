21057a7d81b9b05acabf62486781e231686be898
Moïse DEKI <mose.deki@mediasoftci.com>
2022-03-30 19:32:39 +0000

    Interface de saisie des locations

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NotifService } from 'src/app/core/services/notif.service';
 import { TarificationsService } from 'src/app/core/services/tarifications/tarifications.service';
@@ -20,28 +21,120 @@ modeAppel: any = 'création';
 tarificationTab: any = [];
 viewTable: boolean = false;
   recapTab: any;
+  totalLocation: any={};
+  selectValue: any[];
+  logistik: boolean;
+  selectedCity: any;
+  remisepercent: any;
+  remisemnt: any;
+  remisenewmnt: any;
 
-  constructor(private notifications: NotifService, private user: UserService, private modalService: NgbModal, private tarifService: TarificationsService,) { }
+  locationData: FormGroup;
+
+  constructor(private notifications: NotifService, private user: UserService, private modalService: NgbModal, private tarifService: TarificationsService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.userData = this.user._donnesUtilisateur()[0];
 
-    this.breadCrumbItems = [{ label: 'Eden décoration' }, { label: 'Liste des location', active: true }];
+    this.locationData = this.fb.group({
+      p_details: [],
+      p_nom: [],
+      p_prenoms: [],
+      p_telephone: [],
+      p_email: [],
+      p_description: [],
+      p_date_envoie: [],
+      p_date_retour: [],
+      p_commune_depart: [],
+      p_commune_arrive: [],
+      p_vehicule: [],
+      p_frais: [],
+    });
+
+    this.userData = this.user._donnesUtilisateur()[0];
+    this.breadCrumbItems = [{ label: 'Eden décoration' }, { label: 'Liste des locations', active: true }];
+    this.totalLocation.qteproduits = 0;
+    this.totalLocation.mntTotal = 0;
+    this.totalLocation.mewTotal = 0;
+    
+    //this.selectValue = ['Alaska', 'Hawaii', 'California', 'Nevada', 'Oregon', 'Washington', 'Arizona', 'Colorado', 'Idaho', 'Montana', 'Nebraska', 'New Mexico', 'North Dakota', 'Utah', 'Wyoming', 'Alabama', 'Arkansas', 'Illinois', 'Iowa'];
+    this.selectValue = [
+      { value: 1, label: 'Abobo' },
+        { value: 2, label: 'Koumassi' },
+        { value: 3, label: 'Marcory'},
+        { value: 4, label: 'Yopougon'}
+    ];
+    this.selectedCity = this.selectValue[3];
 
     this._listProduits();
-
   }
 
+  //Calcul de la remise de la remise
+  _calculateRemise(val, typeremise){
+      switch (typeremise) {
+        case 'percent':
+          this.remisemnt = 0;
+          this.remisenewmnt = 0;
+          const valeur1 = parseInt(val, 10) * (1/100);
+          this.totalLocation.mewTotal = this.totalLocation.mntTotal - (this.totalLocation.mntTotal * valeur1);
+          break;
+
+        case 'mntfixe':
+          this.remisepercent = 0;
+          this.remisenewmnt = 0;
+          const valeur2 = parseInt(val, 10);
+          this.totalLocation.mewTotal = this.totalLocation.mntTotal - valeur2;
+          break;
+
+        case 'newmnt':
+          this.remisepercent = 0;
+          this.remisemnt = 0;
+          const valeur3 = parseInt(val, 10);
+          this.totalLocation.mewTotal = valeur3;
+          break;
+      }
+  }
+
+  _test(a){
+    console.log(a);
+    
+  }
+
+  _gestionLogistik(val: boolean): void {
+    this.logistik = val;
+  }
+
+  //sélection quantité par produit
   _valueQte(val,i){
+    this.totalLocation = {};
+    //Modification de la ligne en cours
     this.tarificationTab[i].qte = parseInt(val, 10);
     this.tarificationTab[i].total =  this.tarificationTab[i].qte*this.tarificationTab[i].r_prix_location;
-
+    
     this.recapTab = this.tarificationTab.filter( el => el.qte >= 1);
 
-    const a = this.recapTab.reduce((accumulator, curValue) => accumulator.total + curValue.total, 0)
-    console.log(a)
+    //Calcul de la somme total et du nombre total de produits de la location
+    switch (this.recapTab.length) {
+      case 1:
+        this.totalLocation.mntTotal = this.tarificationTab[i].total;
+        this.totalLocation.qteproduits = this.tarificationTab[i].qte;
+        
+        break;
+    
+      default:
+        
+        //const a = this.recapTab.reduce((a, b) => ({total: a.total + b.total}));
+        const a = this.recapTab.reduce(function (acc, obj) { return acc + obj.total; }, 0);
+        const b = this.recapTab.reduce(function (acc, obj) { return acc + obj.qte; }, 0);
+
+        this.totalLocation.mntTotal = a;
+        this.totalLocation.qteproduits = b;
+        break;
+    }
+    
   }
 
+  //Cocher la ligne produit à sélectionner
   _changeValcheck(val,i){
     this.tarificationTab[i].check = val;
     if(val == false){
@@ -50,6 +143,14 @@ viewTable: boolean = false;
     }
     this.recapTab = this.tarificationTab.filter( el => el.qte >= 1);
 
+  }
+
+
+  _registerLocations(){
+    this.locationData.value.p_details = this.recapTab;
+    this.locationData.value.p_mnt_total = this.totalLocation.mntTotal;
+    console.log(this.locationData.value);
+    
   }
 
   _listProduits(): void {
--- src/app/pages/location/location.module.ts
+++ src/app/pages/location/location.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbDatepickerModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgSelectModule } from "@ng-select/ng-select";
 import { ArchwizardModule } from "angular-archwizard";
 import { NgxMaskModule } from "ngx-mask";
 import { UIModule } from "src/app/shared/ui/ui.module";
@@ -17,6 +19,8 @@ import { LocationComponent } from "./location.component";
         LocationRoutingModule,
         ArchwizardModule,
         NgxMaskModule.forRoot(),
+        NgbDatepickerModule,
+        NgSelectModule
     ]
 })
 
